7b6606e7bdf088c554ed58ca58773e63b2747e8b
vzw83 <vzw83@yandex.ru>
2024-11-27 10:59:03 +0300

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <a href="">sDsa</a>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reacts.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn  React
         </a>
       </header>
     </div>
